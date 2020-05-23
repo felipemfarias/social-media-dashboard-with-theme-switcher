2f4bd2858fc052c309540bed0f6e3c7cd6698965
Felipe Farias <carlosfelipefarias@gmail.com>
2020-05-24 00:36:45 +0200

    Remove unnecessary files

--- index.js
+++
@@ -1,6 +0,0 @@
-let darkModeSwitcher = document.getElementById("dark-mode-switch");
-darkModeSwitcher.onchange = () => {
-  darkModeSwitcher.checked
-    ? document.body.classList.add("dark")
-    : document.body.classList.remove("dark");
-};
